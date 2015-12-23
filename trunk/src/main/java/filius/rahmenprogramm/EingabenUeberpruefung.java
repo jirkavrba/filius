5cd19ce8ee976de5648db502c778907f1fb504f9
sfreischlad <sfreischlad@c1bfa275-6e10-44d3-b15b-780a703ec972>
2015-12-23 13:29:38 +0000

    fix invalid pattern for email addresses

--- trunk/src/main/java/filius/rahmenprogramm/EingabenUeberpruefung.java
+++ trunk/src/main/java/filius/rahmenprogramm/EingabenUeberpruefung.java
@@ -33,113 +33,80 @@ import filius.Main;
 
 /**
  * 
- * Dient dazu Eingaben auf Richtigkeit zu Pruefen. Dazu stehen verschiedene
- * Pattern zur Verfuegung, z.B. fuer IP-Adressen oder Klassen-Namen. Mit diesen
- * Pattern wird die Funktion isGueltig aufgerufen, die den String auf ein Muster
- * prueft.
+ * Dient dazu Eingaben auf Richtigkeit zu Pruefen. Dazu stehen verschiedene Pattern zur Verfuegung, z.B. fuer
+ * IP-Adressen oder Klassen-Namen. Mit diesen Pattern wird die Funktion isGueltig aufgerufen, die den String auf ein
+ * Muster prueft.
  * 
  * @author Johannes Bade
  * 
  */
 public class EingabenUeberpruefung implements I18n {
 
-	// NOTE: include *.*.*.0 to be able to still use this pattern for routing
-	// table configuration as network identifier
-	public static final Pattern musterIpAdresse = Pattern
-	        .compile("^(0*([1-9][0-9]?|1[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.((0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.){2}(0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))$");
-	public static final Pattern musterIpAdresseAuchLeer = Pattern
-	        .compile("^((0*([1-9][0-9]?|1[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.((0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.){2}(0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))){0,1}$");
+    // NOTE: include *.*.*.0 to be able to still use this pattern for routing
+    // table configuration as network identifier
+    public static final Pattern musterIpAdresse = Pattern
+            .compile("^(0*([1-9][0-9]?|1[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.((0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.){2}(0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))$");
+    public static final Pattern musterIpAdresseAuchLeer = Pattern
+            .compile("^((0*([1-9][0-9]?|1[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.((0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.){2}(0*(1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))){0,1}$");
 
-	public static final Pattern musterSubNetz = Pattern
-	        .compile("^(0*([1-9]|1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.((0*([0-9]|1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.){2}(0*([0-9]|1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))$");
-	// public static final Pattern musterEmailAdresse =
-	// Pattern.compile("^[a-zA-Z0-9\\-_][a-zA-Z0-9\\-_\\.]*[a-zA-Z0-9\\-_]@[a-zA-Z0-9][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*$");
-	public static final Pattern musterEmailAdresse = Pattern.compile("^"
-	        + // beginning of line
-	        "("
-	        + // start choice
-	        "[a-z A-Z0-9\\-_\\.]* <"
-	        + // address with name, beginning; Name part
-	        "[a-zA-Z0-9\\-_][a-zA-Z0-9\\-_\\.]*[a-zA-Z0-9\\-_]@[a-zA-Z0-9][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*"
-	        + // standard mail address
-	        ">"
-	        + // address with name, end
-	        "|"
-	        + // choice select
-	        "[a-zA-Z0-9\\-_][a-zA-Z0-9\\-_\\.]*[a-zA-Z0-9\\-_]@[a-zA-Z0-9][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*"
-	        + // standard mail address
-	        ")" + // end choice
-	        // "(, "+ // boundary for multiple mails
-	        // "("+ // start choice
-	        // "[a-z A-Z0-9\\-_\\.]* <"+ // address with name, beginning; Name
-	        // part
-	        // "[a-zA-Z0-9\\-_][a-zA-Z0-9\\-_\\.]*[a-zA-Z0-9\\-_]@[a-zA-Z0-9][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*"+
-	        // // standard mail address
-	        // ">"+ // address with name, end
-	        // "|"+ // choice select
-	        // "[a-zA-Z0-9\\-_][a-zA-Z0-9\\-_\\.]*[a-zA-Z0-9\\-_]@[a-zA-Z0-9][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*"+
-	        // // standard mail address
-	        // ")"+ // end choice
-	        // ")"+ // end boundary
-	        "$");
-	public static final Pattern musterEmailAdresseExt = Pattern
-	        .compile("^[a-z A-Z0-9\\-_\\.]* <[a-zA-Z0-9\\-_][a-zA-Z0-9\\-_\\.]*[a-zA-Z0-9\\-_]@[a-zA-Z0-9][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*>$");
-	public static final Pattern musterKlassenName = Pattern.compile("[A-Z]([a-zA-Z]{2,})?");
-	public static final Pattern musterPort = Pattern
-	        .compile("([1-9]|[1-9][0-9]{1,3}|[1-5][0-9]{1,4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])");
-	public static final Pattern musterPortAuchLeer = Pattern
-	        .compile("(^$|[1-9]|[1-9][0-9]{1,3}|[1-5][0-9]{1,4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])");
-	public static final Pattern musterKeineLeerzeichen = Pattern.compile("[^\\s]*");
-	public static final Pattern musterEmailBenutzername = Pattern.compile("([a-zA-Z0-9]|\\.|\\_|\\-)*");
-	public static final Pattern musterMindEinZeichen = Pattern.compile("(.){1,}");
-	public static final Pattern musterNurZahlen = Pattern.compile("\\d");
-	// public static final Pattern musterDomain =
-	// Pattern.compile("([a-zA-Z0-9]|\\.){2,}");
-	public static final Pattern musterDomain = Pattern
-	        .compile("^([a-zA-Z][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*){0,1}\\.{0,1}$");
-	public static final Pattern musterSubnetBinary = Pattern.compile("^11*0*$");
+    public static final Pattern musterSubNetz = Pattern
+            .compile("^(0*([1-9]|1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.((0*([0-9]|1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))\\.){2}(0*([0-9]|1?[0-9]{1,2}|2[0-4]?[0-9]|25[0-5]))$");
+    private static final String rawMailAddress = "[a-zA-Z0-9]([\\-_\\.]{0,1}[a-zA-Z0-9])*@[a-zA-Z0-9]([\\-_\\.]{0,1}[a-zA-Z0-9])*\\.{0,1}";
+    public static final Pattern musterEmailAdresse = Pattern.compile("^[a-zA-Z0-9 \\-_\\.]* <" + rawMailAddress + ">"
+            + "|" + rawMailAddress + "$");
+    public static final Pattern musterKlassenName = Pattern.compile("[A-Z]([a-zA-Z]{2,})?");
+    public static final Pattern musterPort = Pattern
+            .compile("([1-9]|[1-9][0-9]{1,3}|[1-5][0-9]{1,4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])");
+    public static final Pattern musterPortAuchLeer = Pattern
+            .compile("(^$|[1-9]|[1-9][0-9]{1,3}|[1-5][0-9]{1,4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])");
+    public static final Pattern musterKeineLeerzeichen = Pattern.compile("[^\\s]*");
+    public static final Pattern musterEmailBenutzername = Pattern.compile("([a-zA-Z0-9]|\\.|\\_|\\-)*");
+    public static final Pattern musterMindEinZeichen = Pattern.compile("(.){1,}");
+    public static final Pattern musterNurZahlen = Pattern.compile("\\d");
+    public static final Pattern musterDomain = Pattern
+            .compile("^([a-zA-Z][a-zA-Z0-9\\-_]*(\\.[a-zA-Z0-9][a-zA-Z0-9\\-_]*)*){0,1}\\.{0,1}$");
+    public static final Pattern musterSubnetBinary = Pattern.compile("^11*0*$");
 
-	public static final Color farbeFalsch = new Color(255, 20, 20);
-	public static final Color farbeRichtig = new Color(0, 0, 0);
+    public static final Color farbeFalsch = new Color(255, 20, 20);
+    public static final Color farbeRichtig = new Color(0, 0, 0);
 
-	/**
-	 * Die Funktion isGueltig bekommt einen String und ein Muster (Vorzugsweise
-	 * ein Muster, dass in der Klasse EingabenUeberpruefung vorgegeben ist)
-	 * Moeglicher Aufruf: isGueltig(ipAdresse,
-	 * EingabeUeberpruefung.musterIpAdresse)
-	 * 
-	 * @param zuPruefen
-	 * @param muster
-	 * @return
-	 */
-	public static boolean isGueltig(String zuPruefen, Pattern muster) {
-		Main.debug.println("INVOKED (EingabenUeberpruefung), isGueltig(" + zuPruefen + "," + muster + ")");
-		Matcher m = muster.matcher(zuPruefen);
-		return m.matches();
-	}
+    /**
+     * Die Funktion isGueltig bekommt einen String und ein Muster (Vorzugsweise ein Muster, dass in der Klasse
+     * EingabenUeberpruefung vorgegeben ist) Moeglicher Aufruf: isGueltig(ipAdresse,
+     * EingabeUeberpruefung.musterIpAdresse)
+     * 
+     * @param zuPruefen
+     * @param muster
+     * @return
+     */
+    public static boolean isGueltig(String zuPruefen, Pattern muster) {
+        Main.debug.println("INVOKED (EingabenUeberpruefung), isGueltig(" + zuPruefen + "," + muster + ")");
+        Matcher m = muster.matcher(zuPruefen);
+        return m.matches();
+    }
 
-	public static boolean isValidSubnetmask(String subnet) {
-		Main.debug.println("INVOKED (EingabenUeberpruefung), isValidSubnetmask(" + subnet + ")");
-		String[] token = subnet.split("\\.");
-		String binary = "";
-		Main.debug.println("DEBUG (EingabenUeberpruefung), '" + token + "', length=" + token.length);
-		if (token.length != 4)
-			return false;
-		try {
-			for (int i = 0; i < token.length; i++) {
-				String currBin = Integer.toBinaryString(Integer.parseInt(token[i]));
-				while (currBin.length() < 8)
-					currBin = "0" + currBin;
-				binary += currBin;
-				Main.debug.println("DEBUG (EingabenUeberpruefung), '" + token[i] + "' ~~> binary (" + i + ") = '"
-				        + binary + "'");
-			}
-		} catch (Exception e) {
-			return false;
-		}
-		if (binary.length() == 32 && isGueltig(binary, EingabenUeberpruefung.musterSubnetBinary))
-			return true;
-		return false;
-	}
+    public static boolean isValidSubnetmask(String subnet) {
+        Main.debug.println("INVOKED (EingabenUeberpruefung), isValidSubnetmask(" + subnet + ")");
+        String[] token = subnet.split("\\.");
+        String binary = "";
+        Main.debug.println("DEBUG (EingabenUeberpruefung), '" + token + "', length=" + token.length);
+        if (token.length != 4)
+            return false;
+        try {
+            for (int i = 0; i < token.length; i++) {
+                String currBin = Integer.toBinaryString(Integer.parseInt(token[i]));
+                while (currBin.length() < 8)
+                    currBin = "0" + currBin;
+                binary += currBin;
+                Main.debug.println("DEBUG (EingabenUeberpruefung), '" + token[i] + "' ~~> binary (" + i + ") = '"
+                        + binary + "'");
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        if (binary.length() == 32 && isGueltig(binary, EingabenUeberpruefung.musterSubnetBinary))
+            return true;
+        return false;
+    }
 
 }
